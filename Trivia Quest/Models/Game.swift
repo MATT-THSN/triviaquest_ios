aed025ef4cb041b62001dcdfd7df0517aeaa3fd9
Matt Thompson <matt.thsn@icloud.com>
2022-07-11 08:46:14 +0200

    Add comments to Game Model file.

--- Trivia Quest/Models/Game.swift
+++ Trivia Quest/Models/Game.swift
@@ -5,14 +5,23 @@
 //  Created by Matthew Thompson on 21/06/2022.
 //
 
+//*************************************************
+//*       Game Model File ~ Setup game logic      *
+//*************************************************
+
 import Foundation
 
 struct Game {
+    // What question is the user on?
     private(set) var currentQuestionIndex = 0
+    // Dictionary to hold a question and the corresponding answer chosen.
     private(set) var guesses = [Question: Int]()
+    // Is the game over?
     private(set) var isOver = false
+    // Gather all questions and shuffle them.
     private let questions = Question.allQuestions.shuffled()
     
+    // Determine how many correct or incorrect answers the user has given.
     var guessCount: (correct: Int, incorrect: Int) {
         var count: (correct: Int, incorrect: Int) = (0, 0)
         for (question, guessedIndex) in guesses {
@@ -25,18 +34,22 @@ struct Game {
         return count
     }
     
+    // How many questions are there?
     var numberOfQuestions: Int {
         questions.count
     }
     
+    // What question are we on?
     var currentQuestion: Question {
         questions[currentQuestionIndex]
     }
     
+    // Make a guess for the current question.
     mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
         guesses[currentQuestion] = index
     }
     
+    // Do we have any more questions to answer? Is the game over?
     mutating func updateGameStatus() {
         if currentQuestionIndex < questions.count - 1 {
             currentQuestionIndex += 1
