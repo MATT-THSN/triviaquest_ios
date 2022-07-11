bcb6c860d692ea9385f7d03f49f5d63be1ffc104
Matt Thompson <matt.thsn@icloud.com>
2022-07-11 09:07:03 +0200

    Add comments to QuestionView file.

--- Trivia Quest/Views/QuestionView.swift
+++ Trivia Quest/Views/QuestionView.swift
@@ -5,6 +5,10 @@
 //  Created by Matthew Thompson on 28/06/2022.
 //
 
+//****************************************************************************
+//*         QuestionView File ~ How each question will be presented          *
+//****************************************************************************
+
 import SwiftUI
 
 struct QuestionView: View {
@@ -14,6 +18,7 @@ struct QuestionView: View {
     
     var body: some View {
         VStack {
+            // Display question
             Text(question.questionText)
                 .font(.largeTitle)
                 .bold()
@@ -21,18 +26,22 @@ struct QuestionView: View {
                 .padding()
             Spacer()
             HStack {
+                // Loop through each question's answer options and generate a horizontal line of buttons.
                 ForEach(0..<question.possibleAnswers.count) { answerIndex in
                     Button(action: {
                         print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
                         viewModel.makeGuess(atIndex: answerIndex)
                     }, label: {
+                        // Call ChoiceTextView to follow button template
                         ChoiceTextView(choiceText:
                                         question.possibleAnswers[answerIndex])
                         .background(viewModel.color(forOptionIndex: answerIndex))
                     })
+                    // Disable the buttons after user has made their choice
                     .disabled(viewModel.guessWasMade)
                 }
             }
+            // Display button to go to the next page after a choice is made.
             if viewModel.guessWasMade {
                 Button(action: {
                     viewModel.displayNextScreen()
