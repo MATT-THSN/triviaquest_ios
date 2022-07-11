272a40eab463c28a5c20881a5bec2d8c9dce1007
Matt Thompson <matt.thsn@icloud.com>
2022-07-11 08:57:23 +0200

    Add comments to ScoreView file.

--- Trivia Quest/Views/ScoreView.swift
+++ Trivia Quest/Views/ScoreView.swift
@@ -5,6 +5,10 @@
 //  Created by Matthew Thompson on 29/06/2022.
 //
 
+//****************************************************************************
+//*       ScoreView File ~ Determine how the final score page will look      *
+//****************************************************************************
+
 import SwiftUI
 
 struct ScoreView: View {
@@ -12,22 +16,26 @@ struct ScoreView: View {
     
     var body: some View {
         ZStack {
+            // Set background colour
             GameColor.main.ignoresSafeArea()
             VStack {
                 Spacer()
                 Text("Final Score:")
                     .font(.body)
                     .padding()
+                // Determine score percentage and display.
                 Text("\(viewModel.percentage)%")
                     .font(.system(size: 50))
                     .bold()
                     .padding()
                 Spacer()
                 VStack {
+                    // Determine number of correct and incorrect guesses.
                     Text("\(viewModel.correctGuesses) ✅")
                     Text("\(viewModel.incorrectGuesses) ❌")
                 } .font(.system(size: 30))
                 Spacer()
+                // Pass in BottomTextView to use template button Re-Take-Quiz buttom
                 NavigationLink(destination: GameView(), label: {BottomTextView(str: "Re-Take Quiz")})
             }
         }
@@ -38,6 +46,7 @@ struct ScoreView: View {
 
 struct ScoreView_Previews: PreviewProvider {
     static var previews: some View {
+        // Must pass in number of correct and incorrect guesses
         ScoreView(viewModel: ScoreViewModel(correctGuesses: 8, incorrectGuesses: 2))
     }
 }
